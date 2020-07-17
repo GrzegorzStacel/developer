aef20f8c905b0776d0541c116107c8dc8a7cd23f
Grzegorz Stącel <stacelgrzegorz@gmail.com>
2020-07-17 19:58:01 +0200

    hide / show navigation menu

--- js/main.js
+++ js/main.js
@@ -1,4 +1,26 @@
 $(document).ready(function () {
-    $("#lightgallery").lightGallery(); 
-    
+
+    $("#lightgallery").lightGallery();
+
+    navigation();
 })
+
+function navigation() {
+
+    var menu = document.getElementById('menu');
+    var nav = document.getElementById('nav');
+    // var exit = document.getElementById('exit');
+
+    menu.addEventListener('click', function (e) {
+        nav.classList.toggle('hide-mobile');
+        console.log('show');
+        e.preventDefault();
+    });
+
+    // exit.addEventListener('click', function (e) {
+    //     nav.classList.toggle('hide-mobile');
+    //     console.log('hide');
+    //     e.preventDefault();
+    // });
+
+}
