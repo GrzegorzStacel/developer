361cc5f20833e6a77bfedf3920fd01e522c88c3b
Grzegorz Stącel <stacelgrzegorz@gmail.com>
2020-07-21 18:41:05 +0200

    Przebudowa hamburger menu

--- js/main.js
+++ js/main.js
@@ -2,25 +2,16 @@ $(document).ready(function () {
 
     $("#lightgallery1, #lightgallery2, #lightgallery3, #lightgallery4, #lightgallery5").lightGallery();
 
-    navigation();
+    hamburgerMenu();
 })
 
-function navigation() {
-
-    var menu = document.getElementById('menu');
-    var nav = document.getElementById('nav');
-    // var exit = document.getElementById('exit');
-
-    menu.addEventListener('click', function (e) {
-        nav.classList.toggle('hide-mobile');
-        console.log('show');
-        e.preventDefault();
-    });
-
-    // exit.addEventListener('click', function (e) {
-    //     nav.classList.toggle('hide-mobile');
-    //     console.log('hide');
-    //     e.preventDefault();
-    // });
-
+function hamburgerMenu() {
+    $("#hamburgermenu").on('click', function () {
+        $(this).find(".bar").toggleClass("active");
+        if ($("#hamburgermenu .bar:first").hasClass("active")){
+            $("#topBar").addClass("ON");
+        } else {
+            $("#topBar").removeClass("ON")
+        }
+    })
 }
